9e18ac5515185cffb4a591d40fc4a35d3732f7d7
AVVA <abba501@mail.ru>
2025-08-27 11:12:20 +0300

    27.08.2025

---
+++ src/main/java/com/avva/five/common_2/Singer.java
@@ -0,0 +1,16 @@
+package com.avva.five.common_2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public interface Singer {
+    Logger LOGGER = LoggerFactory.getLogger(Singer.class);
+
+    void  sing();
+
+    default void sing(String key) {
+        LOGGER.info("Singing in the key of {}", key);
+    }
+    default void rest(){
+    }
+}
