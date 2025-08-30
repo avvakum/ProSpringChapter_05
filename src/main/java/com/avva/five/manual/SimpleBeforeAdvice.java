2aff7b0611a7d2f80e7ce9725c85c3457c21104c
AVVA <abba501@mail.ru>
2025-08-30 11:52:43 +0300

    30.08.2025

---
+++ src/main/java/com/avva/five/manual/SimpleBeforeAdvice.java
@@ -0,0 +1,17 @@
+package com.avva.five.manual;
+
+import org.jspecify.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class SimpleBeforeAdvice implements MethodBeforeAdvice {
+    private static Logger LOGGER = LoggerFactory.getLogger(SimpleBeforeAdvice.class);
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        LOGGER.info("Before: set up concert hall.");
+    }
+}
