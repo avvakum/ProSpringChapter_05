bce6eabd93d54276688bfaa3bfcbeeb3478035d6
AVVA <abba501@mail.ru>
2025-09-04 10:45:16 +0300

    04.09.2025

---
+++ src/main/java/com/avva/five/common/GoodGuitarist.java
@@ -0,0 +1,14 @@
+package com.avva.five.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GoodGuitarist implements Singer {
+    private static Logger LOGGER = LoggerFactory.getLogger(GoodGuitarist.class);
+
+    @Override
+    public void sing() {
+        LOGGER.info("Head on your heart, arms around me");
+
+    }
+}
