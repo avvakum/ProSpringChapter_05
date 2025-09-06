3636bf2f3e7dee477e73f9f8914258746dfb6921
AVVA <abba501@mail.ru>
2025-09-06 11:01:14 +0300

    06.09.2025

---
+++ src/main/java/com/avva/five/common_2/GrammyGuitarist.java
@@ -0,0 +1,27 @@
+package com.avva.five.common_2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrammyGuitarist implements Singer {
+    private static Logger LOGGER = LoggerFactory.getLogger(GreatGuitarist.class);
+
+    @Override
+    public void sing() {
+        LOGGER.info("sing: Gravity is working against me\n" +
+                "And gravity wants to bring me down");
+
+    }
+
+    public void sing(Guitar guitar) {
+        LOGGER.info("play: " + guitar.play());
+    }
+
+    public void talk() {
+        LOGGER.info("talk");
+    }
+
+    public void rest() {
+        LOGGER.info("zzz");
+    }
+}
