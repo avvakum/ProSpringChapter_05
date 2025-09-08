1d4dda00171d06fedb2f95496552de01892ef4cc
AVVA <abba501@mail.ru>
2025-09-08 12:19:39 +0300

    08.09.2025

---
+++ src/main/java/com/avva/five/common/GreatGuitarist.java
@@ -0,0 +1,14 @@
+package com.avva.five.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GreatGuitarist implements Singer {
+    private static Logger LOGGER = LoggerFactory.getLogger(GreatGuitarist.class);
+
+    @Override
+    public void sing() {
+        LOGGER.info("You've got my soul in your hand");
+
+    }
+}
