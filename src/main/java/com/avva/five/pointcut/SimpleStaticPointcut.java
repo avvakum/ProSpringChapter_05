dfa35d35d7306342ef331769ddd265be4504e73a
AVVA <abba501@mail.ru>
2025-09-17 11:23:58 +0300

    17.09.2025

---
+++ src/main/java/com/avva/five/pointcut/SimpleStaticPointcut.java
@@ -0,0 +1,20 @@
+package com.avva.five.pointcut;
+
+import com.avva.five.common.GoodGuitarist;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.StaticMethodMatcherPointcut;
+
+import java.lang.reflect.Method;
+
+public class SimpleStaticPointcut extends StaticMethodMatcherPointcut {
+
+    @Override
+    public boolean matches(Method method, Class<?> cls) {
+        return ("sing".equals(method.getName()));
+    }
+
+    @Override
+    public ClassFilter getClassFilter() {
+        return cls -> (cls == GoodGuitarist.class);
+    }
+}
