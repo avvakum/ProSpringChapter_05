14c538c87f8e24ac213d466d722ddf734bbafd49
AVVA <abba501@mail.ru>
2025-10-21 11:56:35 +0300

    21.10.2025

---
+++ src/main/java/com/avva/five/common_2/AuditAdvice.java
@@ -0,0 +1,17 @@
+package com.avva.five.common_2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.MethodBeforeAdvice;
+
+import java.lang.reflect.Method;
+
+public class AuditAdvice implements MethodBeforeAdvice {
+    private static Logger LOGGER = LoggerFactory.getLogger(AuditAdvice.class);
+
+    @Override
+    public void before(Method method, Object[] args, Object target) throws Throwable {
+        LOGGER.info("Executing {}", method);
+
+    }
+}
